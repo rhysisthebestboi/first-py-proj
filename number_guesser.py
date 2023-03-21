14485b9c275f0269bea729a60d184e9be71b9e25
rhysisthebestboi <122556290+rhysisthebestboi@users.noreply.github.com>
2023-03-21 01:02:58 +0000

    hbvghvghvghvhjgvb

--- number_guesser.py
+++ number_guesser.py
@@ -1,2 +1,58 @@
+import random
 
-print("guess a number from 1 to 10.")
+print("guess a number from 1 to 10. you have 5 guess's")
+
+value = random.randint(1, 10)
+
+guess = int(input("what is your guess: "))
+
+if guess > value:
+    print("too high")
+elif guess < value:
+    print("too low")
+else:
+    print("you won!")
+    quit()
+
+guess = int(input("what is your guess: "))
+
+if guess > value:
+    print("too high")
+elif guess < value:
+    print("too low")
+else:
+    print("you won!")
+    quit()
+
+guess = int(input("what is your guess: "))
+
+if guess > value:
+    print("too high")
+elif guess < value:
+    print("too low")
+else:
+    print("you won!")
+    quit()
+    
+guess = int(input("what is your guess: "))
+
+if guess > value:
+    print("too high")
+elif guess < value:
+    print("too low")
+else:
+    print("you won!")
+    quit()
+
+guess = int(input("what is your guess: "))
+
+if guess > value:
+    print("too high")
+elif guess < value:
+    print("too low")
+else:
+    print("you won!")
+    quit()
+
+print("you lost")
+quit()
